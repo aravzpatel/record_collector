6e4bd00437446273d1048adf4b96c7e2ee50bb2e
Arav <arav.patel@gmail.com>
2020-05-15 14:02:44 +0100

    Have updated the file system ot make it cleaner

--- discogs.sh
+++ discogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lynx -cmd_script=./record_script2 -accept_all_cookies http://www.google.co.uk/search?q=discogs+$1
+lynx -cmd_script=./record_script1 -accept_all_cookies http://www.google.co.uk/search?q=discogs+$1
 
 grep -A 10 Statistics 1 > $1.txt
 rm 1
